4a6acf468bd4ca81851ecff8714af308f0cffeb8
Anssi <anssi.valjakka@gmail.com>
2022-01-08 13:19:28 +0200

    It works

---
+++ run.py
@@ -0,0 +1,27 @@
+from json import loads
+import argparse
+
+from urllib.request import urlopen
+from pprint import pprint
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("timezone", help="Your timezone e.g. -7", type=int)
+    parser.add_argument("sunset", help="Desired sunset time in format hh:mm", 
+                        type=str)
+    args = parser.parse_args()
+    timezone = args.timezone
+    sunset_string = args.sunset
+    sunset = int(sunset_string.split(":")[0])*60 + int((sunset_string.split(":")[1]))
+    lng = get_longitude_for_sunset((sunset-timezone*60))
+    print(lng)
+
+def get_longitude_for_sunset(sunset): # sunset is time in minutes from midnight
+    tmp = -90-15/60*sunset
+    if(tmp<-180):
+        tmp+=360
+    return(tmp)
+
+if __name__ == '__main__':
+    main()
